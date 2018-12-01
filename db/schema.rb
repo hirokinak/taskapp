0a941589bd1f7aebcd89c295b698ce88cfffa9b7
hirokinak <hiroki.srv66@gmail.com>
2018-12-01 16:10:00 +0900

    step７まで。タスクを登録・更新・削除作成

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 2018_11_25_071614) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
+    t.string "name", null: false
+    t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
