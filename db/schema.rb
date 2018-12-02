abb55b35a62d00839b127b6246e4b71a3c1df117
hirokinak <hiroki.srv66@gmail.com>
2018-12-02 20:33:46 +0900

    step12まで（バリデーション設定）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_071614) do
+ActiveRecord::Schema.define(version: 2018_12_02_111436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
