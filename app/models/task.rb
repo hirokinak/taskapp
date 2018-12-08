664e8b0566309a8098a20c097825ae704595bf45
hirokinak <hiroki.srv66@gmail.com>
2018-12-09 06:35:00 +0900

    step14途中まで　status追加　日時表示変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  enum status:[:draft, :doing, :done]
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { maximum: 200 }
 end
