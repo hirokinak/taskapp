a93bac516dae47f9a2e8699980480eaebb00902f
hirokinak <hiroki.srv66@gmail.com>
2018-12-10 21:27:49 +0900

    step16まで　優先順位（高中低）追加、ソート実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
-  enum status:[:draft, :doing, :done]
+  enum status: {"未着手": 0, "実行中": 1,  "完了": 2}
+  enum priority: {"低": 0, "中": 10, "高": 20}
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { maximum: 200 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_08_204513) do
+ActiveRecord::Schema.define(version: 2018_12_09_102650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_12_08_204513) do
     t.datetime "updated_at", null: false
     t.datetime "deadline"
     t.integer "status"
+    t.integer "priority"
   end
 
 end
