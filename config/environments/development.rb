cba6b6dafa1a486cfaf96771259664fa662260fe
hirokinak <hiroki.srv66@gmail.com>
2018-12-23 11:34:07 +0900

    step22途中まで　ロール選択

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -67,4 +67,5 @@ Rails.application.configure do
     Bullet.rails_logger = true 
     Bullet.add_footer   = true 
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :tasks
   root to: 'tasks#index'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
