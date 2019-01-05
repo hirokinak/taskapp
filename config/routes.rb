e6e41a275052d4be6af1a806585eccc4e8a54d21
hirokinak <hiroki.srv66@gmail.com>
2019-01-06 07:35:44 +0900

    step24 rescue_fromでエラーページ表示

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :tasks
   root to: 'tasks#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match "*path" => "application#handle_404", via: :all
 end
